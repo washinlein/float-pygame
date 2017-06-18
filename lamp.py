b7512736b17c12ea643470d05bf233c6bc8a4c28
Washinlein <washinlein@gmail.com>
2017-06-18 14:37:40 +0200

    Fixed lamp surface transparency not being applied. Added new room tiles. Lamp sprite updated. Some code moved to load image after pygame initialization.

--- lamp.py
+++ lamp.py
@@ -7,18 +7,21 @@ class Lamp:
     __IMAGE_PATH = './assets/graphics/lamp_sprites.png'
     LAMP_LIT = 0
     LAMP_UNLIT = 1
-    __frames = SpriteSheet(__IMAGE_PATH, (25, 25)).frames
-    sound_pickup = None
 
+    sound_pickup = None
     current_order = 0
+    _current_index = 0
 
     def __init__(self, rect, order):
         self.surface = None
         self.rect = rect
         self.order = int(order)
         self.sound_pickup = self.sound_pickup = pygame.mixer.Sound(self.__SOUND_PICKUP)
+        self.__frames = SpriteSheet(self.__IMAGE_PATH, (25, 25)).frames
         self.reset()
-
+        self.index = Lamp._current_index
+        Lamp._current_index += 1
+        print "Index %s is order %s " % (self.index, self.order)
     def lit(self):
         self.surface = self.__frames[self.LAMP_LIT]
 
--- sprite_sheet.py
+++ sprite_sheet.py
@@ -14,6 +14,6 @@ class SpriteSheet:
         for y in range(0, self.image_surface.get_height(), self.__sprite_height):
             for x in range(0, self.image_surface.get_width(), self.__sprite_width):
                 r = pygame.Rect(x, y, self.__sprite_width, self.__sprite_height)
-                tmp_surface = pygame.Surface((self.__sprite_width, self.__sprite_height), flags=pygame.HWSURFACE)
+                tmp_surface = pygame.Surface((self.__sprite_width, self.__sprite_height), pygame.SRCALPHA)
                 tmp_surface.blit(self.image_surface, (0, 0), r)
                 self.frames.append(tmp_surface)
